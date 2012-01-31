0bfb41573df33c275d6206da8fe107c98fb998c5
Mike Kolganov <mike.kolganov@gmail.com>
2012-02-01 01:41:56 +0700

    First commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_timeentry_extensions do
+  name 'Redmine Timeentry Extensions plugin'
+  author 'Mike Kolganov, Thumbtack Inc.'
+  description 'Timeentry extensions'
+  version '0.0.1'
+  #url 'http://example.com/path/to/plugin'
+  #author_url 'http://example.com/about'
+end
