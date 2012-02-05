29421d70abacdbe1345db84bf1b4ca895829c80c
Mike Kolganov <mike.kolganov@gmail.com>
2012-02-06 02:20:51 +0700

    User selector in timeentry form for admins

--- assets/javascripts/time_entry.js
+++ assets/javascripts/time_entry.js
@@ -3,13 +3,18 @@ document.observe('dom:loaded', function(){
   var TimeEntryFormControl = Class.create(time_entry_options, {
     
     initialize: function() {
-      if(this.frozen_time_entry) {
-        this.lock_timeentry_form();
+      this.translator = new Translator(this.i18n_strings);
+      if (this.is_admin) {
+        this.attach_users_selector();
       }
-      else if(this.frozed_deadline) {
-        this.lock_js_calendar(this.frozed_deadline);
-      }
-      
+      else {
+        if(this.frozen_time_entry) {
+          this.lock_timeentry_form();
+        }
+        else if(this.frozed_deadline) {
+          this.lock_js_calendar(this.frozed_deadline);
+        } 
+      }      
     },
     
     lock_js_calendar: function(date) {
@@ -33,9 +38,74 @@ document.observe('dom:loaded', function(){
       $$('#content form').first().select('.calendar-trigger').each(function(element){
         element.onclick = null;
       });
+    },
+    
+    attach_users_selector: function() {
+      var label = new Element('label', {'for': 'user_id'})
+        .update(this._('field_user'));
+      var select = new Element('select', {name: 'time_entry[user_id]'});
+      new Hash(this.users).each(function(pair){
+        var user_id = parseInt(pair.first(), 10);
+        var user_name = pair.last();
+        var option = new Element('option', {value: user_id})
+          .update(user_name);
+        select.insert(option);
+      });
+      select.value = this.time_entry.user_id;
+      var user_field = new Element('p')
+        .insert(label)
+        .insert(select);
+        
+      $$('#content div.box').first().insert({
+        top: user_field
+      });
+    },
+    
+    /**
+     * Wrapper for Translator.get() method
+     * Translate i18n ID to string for current language
+     *
+     * @param  key      i18n ID
+     * @param  get_back Get back i18n ID if translation not exists
+     * 
+     * @return Translated string for current language
+     */
+    _: function(key, get_back) {
+      return this.translator.get(key, get_back);
     }
     
-  })
+  });
+  
+  /**
+   * Translator
+   */
+  var Translator = Class.create({
+    /**
+     * Constructor.
+     * Initialize translator
+     *
+     * @param  i18n strings class object
+     *
+     * @return void
+     */
+    initialize: function(i18n_strings) {
+      this.i18n_strings = new Hash(i18n_strings);
+    },
+
+    /**
+     * Translate string using IssueHotButtonsSettings locale strings store
+     *
+     * @param  key i18n identifier
+     * @param  get_back Get back i18n ID if translation not exists
+     *
+     * @return Translated string or input key if translation not found
+     */
+    get: function(key, get_back) {
+      get_back = get_back === false ? false : true;
+      if (Object.isArray(key)) key = key.join('_');
+      return this.i18n_strings.get(key) || (get_back ? key : false);
+    }
+  });
   
   new TimeEntryFormControl();
   
