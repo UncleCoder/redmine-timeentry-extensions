52fe67e55c59da4e15466f333e9396e6f6e1316a
Mike Kolganov <mike.kolganov@gmail.com>
2012-02-05 20:01:43 +0700

    Dead-line settings

--- init.rb
+++ init.rb
@@ -1,11 +1,11 @@
 require 'redmine'
 
-Redmine::Plugin.register :redmine_timeentry_extensions do
+Redmine::Plugin.register :timeentry_extensions do
   name 'Redmine Timeentry Extensions plugin'
   author 'Mike Kolganov, Thumbtack Inc.'
   description 'Timeentry extensions'
   version '0.0.1'
   #url 'http://example.com/path/to/plugin'
   #author_url 'http://example.com/about'
-  settings :partial => 'settings/settings'
+  settings :partial => 'settings/timeentry_extensions_settings'
 end
